fd0eb7fc7e129aec25453ba43505e8f9d7ca0c13
Glitch <iGlitch@users.noreply.github.com>
2020-03-13 01:52:16 -0400

    Create ja.js

---
+++ src/translations/ja.js
@@ -0,0 +1,44 @@
+export default {
+  app: {
+    links: {
+      support: {
+        title: 'Discordでサポートが受けられます',
+        description: '何か問題がありますか？ Discordで開発者に直接連絡して下さい。'
+      },
+      mirror: {
+        title: 'ミラーサイト',
+        description: 'お探しのファイルが見つかりませんか？ ミラーサイトで検索してみて下さい。'
+      }
+    },
+    searchBar: {
+      inline: 'キーワードから探す...',
+      button: '検索'
+    }
+  },
+  api: {
+    loading: 'APIからの応答を待っている...',
+    ifLoadTooLong: 'もしアプリケーションに時間がかかりすぎた場合にはページを再読み込みして下さい。'
+  },
+  search: {
+    title: '検索',
+    description: '\'%s\'の検索結果。'
+  },
+  item: {
+    parsingFailed: 'このファイルの名前の解析中に問題が発生しました。',
+    viewSeries: '全エピソード'
+  },
+  list: {
+    previous: '前へ',
+    next: '次へ',
+    latest: '最新',
+    page: 'ページ'
+  },
+  series: {
+    status: '放映状況',
+    statusTypes: {
+      FINISHED: '放映終了',
+      RELEASING: '放映中'
+    },
+    description: 'あらすじ'
+  }
+}
