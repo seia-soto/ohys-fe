c4f3fe3a489ece564fa98b6ed90a743077cca09e
Seia-Soto <seia@outlook.kr>
2020-03-13 13:33:29 +0900

    Add excepetion for settings.language

--- src/structures/store/initialize.js
+++ src/structures/store/initialize.js
@@ -1,9 +1,17 @@
+import translations from '../../translations'
+
 const localStorage = window.localStorage
+
+// NOTE: Language exceptions
+const languageAvailable = Object.keys(translations)
 
 const reset = () => {
   const settings = {}
 
   settings.language = (navigator.language || navigator.userLanguage || 'en').slice(0, 2)
+
+  // NOTE: Handle exceptions.
+  if (!languageAvailable.includes(settings.language)) settings.language = 'en'
 
   localStorage.setItem('settings', JSON.stringify(settings))
 
