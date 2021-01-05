a033073c16c5a8b3d447b288190efbe9c187290c
Seia-Soto <seia@outlook.kr>
2021-01-06 07:35:02 +0900

    Add IE9 support and update build options

--- snowpack.config.js
+++ snowpack.config.js
@@ -1,10 +1,30 @@
 /** @type {import("snowpack").SnowpackUserConfig } */
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+
 module.exports = {
   mount: {
     public: { url: '/', static: true },
     src: { url: '/dist' }
   },
-  plugins: ['@snowpack/plugin-react-refresh', '@snowpack/plugin-dotenv'],
+  plugins: [
+    '@snowpack/plugin-react-refresh',
+    '@snowpack/plugin-dotenv',
+    [
+      '@snowpack/plugin-webpack',
+      {
+        extendConfig: config => {
+          config.plugins.push(new CleanWebpackPlugin({
+            dry: false,
+            verbose: true,
+            cleanStaleWebpackAssets: true,
+            protectWebpackAssets: true
+          }))
+
+          return config
+        }
+      }
+    ]
+  ],
   install: [
     /* ... */
   ],
@@ -15,7 +35,7 @@ module.exports = {
     /* ... */
   },
   buildOptions: {
-    /* ... */
+    clean: true
   },
   proxy: {
     /* ... */
